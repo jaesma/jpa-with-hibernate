29c498a294eee787573b62ed30331d465c177f8f
jaesma <jamesmanalang11392@gmail.com>
2019-07-14 09:41:34 +0800

    Initial Commit. 1.) Create entity class. (POJO) Course class

---
+++ src/main/java/com/manalangjames/JpaWithHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.manalangjames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWithHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaWithHibernateApplication.class, args);
+    }
+
+}
